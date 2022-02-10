79e6db5847c4f8881f0d90643c3fd1188d63889a
Muchammad Amru Al-Chakimi <amrualchakim@gmail.com>
2022-02-10 11:12:39 +0700

    kemajuan sedikit

--- foody/lib/home.dart
+++ foody/lib/home.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:foody/theme.dart';
 
-import 'icons/my_icons_icons.dart';
 
 class Home extends StatefulWidget {
   const Home({ Key? key }) : super(key: key);
@@ -22,12 +21,12 @@ class _HomeState extends State<Home> {
               padding: EdgeInsets.only(
                 top: 30,
                 left: 15,
-                right: 15
+                right: 15,
               ),
               decoration: BoxDecoration(
                 color: biruTua,
                 borderRadius: BorderRadius.vertical(
-                  bottom: Radius.circular(50),
+                  bottom: Radius.circular(70),
                 ),
               ),
               child: Column(
@@ -41,39 +40,107 @@ class _HomeState extends State<Home> {
                         }, 
                         icon: Icon(
                           Icons.arrow_back,
-                          size: 24,
+                          size: 30,
                           color: Colors.white,
-                        ),
+                        )
                       ),
+                      //buat bikin teks input pencarian
                       IconButton(
                         onPressed: (){
                           
                         }, 
                         icon: Icon(
                           Icons.shopping_cart_outlined,
-                          size: 24,
                           color: Colors.white,
-                        ),
-                      ),
+                          size: 30,
+                        )
+                      )
                     ],
                   ),
-                  SizedBox(height: 70,),
+                  SizedBox(height: 30,),
                   Row(
                     children: [
                       Padding(
                         padding: EdgeInsets.only(
-                          left: 30
+                          left: 15,
                         )
                       ),
-                      Text('kuy')
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Hello, Nagib',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 23,
+                              fontWeight: FontWeight.w700
+                            ),
+                          ),
+                          SizedBox(height: 5,),
+                          Text(
+                            'What do you want to eat',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 15
+                            ),
+                          ),
+                          Text(
+                            ' right now?',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 15
+                            ),
+                          )
+                        ],
+                      ),
+                      Spacer(),
+                      Image.asset(
+                        'assets/images/human.png',
+                        height: 170,
+                        width: 170,
+                      )
                     ],
                   ),
                   SizedBox(height: 30,)
                 ],
               ),
             ),
+            SizedBox(height: 20,),
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
+                ActionChip(
+                  //avatar: Icon(Icons.favorite),
+                  label: Text(
+                    'Rice',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  onPressed: () {},
+                  backgroundColor: biruTua,
+                ),
+                ActionChip(
+                  //avatar: Icon(Icons.favorite),
+                  label: Text('Noodle'),
+                  onPressed: () {},
+                ),
+                ActionChip(
+                  //avatar: Icon(Icons.favorite),
+                  label: Text('Chicken'),
+                  onPressed: () {},
+                ),
+                ActionChip(
+                  //avatar: Icon(Icons.favorite),
+                  label: Text('Soup'),
+                  onPressed: () {},
+                ),
+                ActionChip(
+                  //avatar: Icon(Icons.favorite),
+                  label: Text('Coffee'),
+                  onPressed: () {},
+                ),
               ],
             )
           ],
